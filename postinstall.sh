b6d36ce0e5d358a10f4886c6fdf2ca5aa3931685
milosponj <milosponj@yahoo.com>
2022-04-09 02:22:15 +0200

    post install fix

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mkdir -p /tmp/fonts
 curl -s "https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf" -L -o /tmp/fonts/NotoColorEmoji.ttf && \
 mkdir -p ./fonts && mv /tmp/fonts/*.ttf ./fonts
